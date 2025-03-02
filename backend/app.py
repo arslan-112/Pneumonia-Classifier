6f656c8c04ff12ed421e34387f161603bdde46fe
DiscoStrangler69 <ijazarslan372@gmail.com>
2025-03-02 17:19:15 +0500

    Updated flask port

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify
 from PIL import Image
 import io
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for frontend communication
@@ -45,5 +46,7 @@ def predict():
 
     return jsonify({'prediction': prediction})
 
+PORT = int(os.environ.get("PORT", 10000))  
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    app.run(host="0.0.0.0", port=PORT, debug=True)
